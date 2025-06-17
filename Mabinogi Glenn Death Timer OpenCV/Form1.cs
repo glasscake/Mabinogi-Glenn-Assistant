16555a35e339eef7348cf57f5256307e5907ae39
glasscake <memodaman@yahoo.com>
2025-06-16 19:44:39 -0700

    Dotnet Project Created

---
+++ Mabinogi Glenn Death Timer OpenCV/Form1.cs
@@ -0,0 +1,10 @@
+namespace Mabinogi_Glenn_Death_Timer_OpenCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
